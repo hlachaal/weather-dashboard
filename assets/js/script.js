53d84e3e2ec8bc52c6938ca8ee831d1bf7ef3551
Hamdi Lachaal <hamdi.lachaal@gmail.com>
2022-03-05 15:19:47 -0700

    added css media queries and screen resizing jQuery code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
-(function ($) {
-
-    "use strict";
-
+$(document).ready(function () {
     var fullHeight = function () {
-
         $('.js-fullheight').css('height', $(window).height());
         $(window).resize(function () {
             $('.js-fullheight').css('height', $(window).height());
         });
-
     };
     fullHeight();
-
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
+    ($(window).width() < 992) ? $('#body').addClass('flex-column-reverse') : $('#body').addClass('flex-row');
+});
 
-})(jQuery);
+$(window).on('resize', function () {
+    var win = $(this);
+    if (win.width() < 992) {
+        $('#body').removeClass('flex-row');
+        $('#body').addClass('flex-column-reverse');
+    } else {
+        $('#body').removeClass('flex-column-reverse');
+        $('#body').addClass('flex-row');
+    }
+});
